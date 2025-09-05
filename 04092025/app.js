aba0393d0dab0dfd50e4298208dca251b8942d81
Juander05 <juandrobles0504@gmail.com>
2025-09-04 18:30:15 -0600

    app y app2 update

---
+++ 04092025/app.js
@@ -0,0 +1,9 @@
+var frutas = ["Pera","manzan","platano","pera"];
+
+for( let fruta of frutas ){
+    console.log(fruta);
+}
+
+for( let fruta in frutas ){
+    console.log(fruta);
+}
---
+++ 04092025/app2.js
@@ -0,0 +1,28 @@
+/*
+saludar();
+function saludar (){
+
+    console.log("Hola mundo desde al funcion");
+
+}
+*/
+
+/*
+console.log(saludar());
+console.log(saludar);
+saludar;
+saludar("Saludar");
+console.log(function saludar());
+console.log(function saludar);
+*/
+
+
+
+
+function saludo( nombreUsuario){
+    //console.log("Hola como estas "+nombreUsuario);
+    alert("Hola como estas "+nombreUsuario);
+}
+
+let nombre = prompt("Nombre: ");
+saludo(nombre);
