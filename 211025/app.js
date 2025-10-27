d14989f9064b7db353eabd5ead8e9aa0f97cf5ec
Juander05 <juandrobles0504@gmail.com>
2025-10-27 02:10:54 -0600

    Pagina QR

---
+++ 211025/app.js
@@ -0,0 +1,38 @@
+let ultimoTextoQR = "";
+
+function generarQR(){
+    let input1= document.getElementById('input1').value.trim();
+    let input2= document.getElementById('input2').value.trim();
+    let input3= document.getElementById('input3').value.trim();
+    let input4= document.getElementById('input4').value.trim();
+
+
+    let textoCompleto = input1+ " " +input2+ " " +input3+" "+input4;
+
+    if( textoCompleto === "" ){
+        alert("Ingresa almenos un texto para generar el QR");
+    }
+
+    if (textoCompleto === ultimoTextoQR) {
+        alert("Ya generaste este mismo código QR");
+        return;
+    }
+
+    ultimoTextoQR = textoCompleto;
+
+    let qrCodeURL='https://api.qrserver.com/v1/create-qr-code/?size=350x200&data='+encodeURIComponent(textoCompleto);
+
+
+    let qrImg = document.createElement('img');
+
+    qrImg.src=qrCodeURL;
+    qrImg.alt='Un cdigo QR';
+
+    console.log(qrImg.src);
+
+    qrImg.classList.add('qr-code');
+
+    document.getElementById('qr-code').appendChild(qrImg);
+        
+
+}
